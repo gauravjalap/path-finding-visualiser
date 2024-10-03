046202b62ea12f9501c6868f8b9641d7546aae4d
gauravjalap <luckyskchirag@gmail.com>
2024-10-03 12:05:09 +0530

    Add Vercel configuration and update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Pathfinding-Visualizer/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+  },
 });
